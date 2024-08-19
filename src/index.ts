44463a53fe5f1d87f78692bb80528d2e61f54509
Mars Liu <mars.liu@outlook.com>
2024-08-19 18:06:53 +0800

    widgets demo for customed notebook header

--- src/index.ts
+++ src/index.ts
@@ -1,171 +1,71 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin,
-  ILayoutRestorer
-} from '@jupyterlab/application';
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
-
-import {
-  ICommandPalette,
-  MainAreaWidget,
-  WidgetTracker
-} from '@jupyterlab/apputils';
 import { Widget } from '@lumino/widgets';
 
-class APODWidget extends Widget {
-  /**
-   * Construct a new APOD widget.
-   */
-  constructor() {
-    super();
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
-    this.addClass('litchiWidget');
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-    // Add an image element to the panel
-    this.img = document.createElement('img');
-    this.node.appendChild(this.img);
+import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
 
-    // Add a summary element to the panel
-    this.summary = document.createElement('p');
-    this.node.appendChild(this.summary);
-  }
-
-  /**
-   * The image element associated with the widget.
-   */
-  readonly img: HTMLImageElement;
-
-  /**
-   * The summary text element associated with the widget.
-   */
-  readonly summary: HTMLParagraphElement;
-
-  /**
-   * Handle update requests for the widget.
-   */
-  async updateAPODiImage(): Promise<void> {
-
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`);
-
-    if (!response.ok) {
-      const data = await response.json();
-      if (data.error) {
-        this.summary.innerText = data.error.message;
-      } else {
-        this.summary.innerText = response.statusText;
-      }
-      return;
-    }
-
-    const data = (await response.json()) as APODResponse;
-
-    if (data.media_type === 'image') {
-      // Populate the image
-      this.img.src = data.url;
-      this.img.title = data.title;
-      this.summary.innerText = data.title;
-      if (data.copyright) {
-        this.summary.innerText += ` (Copyright ${data.copyright})`;
-      }
-    } else {
-      this.summary.innerText = 'Random APOD fetched was not an image.';
-    }
-  }
-
-  /**
-   * Get a random date string in YYYY-MM-DD format.
-   */
-  randomDate(): string {
-    const start = new Date(2010, 1, 1);
-    const end = new Date();
-    const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    return randomDate.toISOString().slice(0, 10);
-  }
-}
-
-interface APODResponse {
-  copyright: string;
-  date: string;
-  explanation: string;
-  media_type: 'video' | 'image';
-  title: string;
-  url: string;
-}
-
+/**
+ * The plugin registration information.
+ */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'litchi-apod',
-  autoStart: true,
-  requires: [ICommandPalette],
-  optional: [ILayoutRestorer, ISettingRegistry],
-  activate: activate
+  activate,
+  id: 'my-extension-name:widgetPlugin',
+  description: 'Add a widget to the notebook header.',
+  autoStart: true
 };
 
 /**
- * Initialization data for the litchi extension.
+ * A notebook widget extension that adds a widget in the notebook header (widget below the toolbar).
  */
-function activate(
-  app: JupyterFrontEnd,
-  palette: ICommandPalette,
-  restorer: ILayoutRestorer | null,
-  settingRegistry: ISettingRegistry | null
-) {
-  console.log('JupyterLab extension litchi is activated!');
-  console.log('ICommandPalette:', palette);
+export class WidgetExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
+  /**
+   * Create a new extension object.
+   */
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const widget = new Widget({ node: Private.createNode() });
+    widget.addClass('jp-myextension-myheader');
 
-  const newWidget = () => {
-    const content = new APODWidget();
-    const widget = new MainAreaWidget({ content });
-    widget.id = 'litchi-settings';
-    widget.title.label = 'Litchi Settings';
-    widget.title.closable = true;
-    return widget;
-  };
-
-  let widget = newWidget();
-  // Add an application command
-  const command: string = 'litchi:settings';
-  app.commands.addCommand(command, {
-    label: 'litchi:settings',
-    execute: async () => {
-      // Regenerate the widget if disposed
-      if (widget.isDisposed) {
-        widget = await newWidget();
-      }
-      if (!widget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.add(widget, 'main');
-      }
-      // Refresh the picture in the widget
-      widget.content.updateAPODiImage();
-      // Activate the widget
-      app.shell.activateById(widget.id);
-    }
-  });
-  // Add the command to the palette.
-  palette.addItem({ command, category: 'Litchi' });
-
-  // if (settingRegistry) {
-  //   settingRegistry
-  //     .load(plugin.id)
-  //     .then(settings => {
-  //       console.log('litchi settings loaded:', settings.composite);
-  //     })
-  //     .catch(reason => {
-  //       console.error('Failed to load settings for litchi.', reason);
-  //     });
-  // }
-
-  // Track and restore the widget state
-  const tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
-    namespace: 'litchi'
-  });
-  if (restorer) {
-    restorer.restore(tracker, {
-      command,
-      name: () => 'litchi'
+    panel.contentHeader.insertWidget(0, widget);
+    return new DisposableDelegate(() => {
+      widget.dispose();
     });
   }
 }
 
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterFrontEnd): void {
+  app.docRegistry.addWidgetExtension('Notebook', new WidgetExtension());
+}
+
+/**
+ * Export the plugin as default.
+ */
 export default plugin;
+
+/**
+ * Private helpers
+ */
+namespace Private {
+  /**
+   * Generate the widget node
+   */
+  export function createNode(): HTMLElement {
+    const span = document.createElement('span');
+    span.textContent = 'My custom header';
+    return span;
+  }
+}
